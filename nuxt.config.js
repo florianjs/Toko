ab02ea755b1a2027fb60b66519472d9fe8f80624
Florian <argaudflorian@gmail.com>
2021-07-04 16:23:56 +0200

    feat: add plausible and .env example

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ export default {
         src: 'https://plausible.io/js/plausible.js',
         async: true,
         defer: true,
-        'data-domain': 'toko-demo.netlify.app',
+        'data-domain': process.env.PLAUSIBLE_DOMAIN,
       },
     ],
     htmlAttrs: {
@@ -25,7 +25,6 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
